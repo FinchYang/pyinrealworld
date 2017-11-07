83a33c9866a1d50e52fdf6465eab3b783a6496c9
hedulewei <yang_clin@aliyun.com>
2017-11-07 10:58:28 +0800

    py redux all

--- trafficpolice/Controllers/historyController.cs
+++ trafficpolice/Controllers/historyController.cs
@@ -29,9 +29,9 @@ namespace trafficpolice.Controllers
         {
             _log = log;
         }
-        [Route("GetHistoryDataNine")]//获取历史每日点名数据查询接口
-        [HttpGet]//9点数据
-        public commonresponse GetHistoryDataNine(string startdate, string enddate,signtype st=signtype.unknown,string reportname="nine")
+        [Route("GetHistoryData")]//获取历史数据查询接口
+        [HttpGet]//
+        public commonresponse GetHistoryData(string startdate, string enddate,signtype st=signtype.unknown,string reportname="")
         {
             var start = DateTime.Now;
             var end = start;
@@ -66,72 +66,74 @@ namespace trafficpolice.Controllers
                 return new commonresponse { status = responseStatus.processerror, content = ex.Message };
             }
         }
-        [Route("GetHistoryData")]//获取历史交管动态历史数据查询接口
-        [HttpGet]//4点数据
-        public commonresponse GetHistoryData(string  startdate,string enddate,string  reportname="four")
-        {
-            var start = DateTime.Now; 
-            var end = start;
-            if(!DateTime.TryParse(startdate,out start))
-            {
-                return global.commonreturn(responseStatus.startdateerror);
-            }
-            if (!DateTime.TryParse(enddate, out end))
-            {
-                return global.commonreturn(responseStatus.enddateerror);
-            }
-            var accinfo = global.GetInfoByToken(Request.Headers);
-            if (accinfo.status != responseStatus.ok) return accinfo;
-            var ret = new hisdatares
-            {
-                status = 0,
-                hisdata = new List<onedata>(),
-                  yearOverYeardata = new List<onedata>(),
-                linkRelativedata = new List<onedata>()
-            };
-         //   var today = DateTime.Now.ToString("yyyy-MM-dd");
-            try
-            {
-                ret.hisdata = gethisdata(start, end, accinfo.unitid,reportname);
-                ret.yearOverYeardata = gethisdata(start.AddYears(-1), end.AddYears(-1), accinfo.unitid, reportname);
-                var days = end.Subtract(start).Days;
-                ret.linkRelativedata = gethisdata(start.AddDays(-1).AddDays(-days), start.AddDays(-1), accinfo.unitid, reportname);
-                return ret;
-            }
-            catch (Exception ex)
-            {
-                _log.LogError("{0}-{1}-{2}", DateTime.Now, "GetHistoryData", ex.Message);
-                return new commonresponse { status = responseStatus.processerror, content = ex.Message };
-            }
-        }     
-        private List<onedata> gethisdata(DateTime sd,DateTime  ed,string unitid,string reportname)
-        {
-            var start = sd.ToString("yyyy-MM-dd");
-            var end = ed.ToString("yyyy-MM-dd");
-            var ret = new List<onedata>();
-            var data = _db1.Reportsdata.Where(c => c.Date.CompareTo(start) >= 0
-         && c.Date.CompareTo(end) <= 0&& c.Rname==reportname
-         && c.Unitid == unitid);
-            foreach (var d in data)
-            {
-                var one = new onedata();
-                one = JsonConvert.DeserializeObject<onedata>(d.Content);
-                one.date = d.Date;
-                one.submittime = d.Submittime.GetValueOrDefault();
-                one.createtime = d.Time;
-                ret.Add(one);
-            }
-            return ret;
-        }
+        //[Route("GetHistoryData")]//获取历史交管动态历史数据查询接口
+        //[HttpGet]//4点数据
+        //public commonresponse GetHistoryData(string  startdate,string enddate,string  reportname="four")
+        //{
+        //    var start = DateTime.Now; 
+        //    var end = start;
+        //    if(!DateTime.TryParse(startdate,out start))
+        //    {
+        //        return global.commonreturn(responseStatus.startdateerror);
+        //    }
+        //    if (!DateTime.TryParse(enddate, out end))
+        //    {
+        //        return global.commonreturn(responseStatus.enddateerror);
+        //    }
+        //    var accinfo = global.GetInfoByToken(Request.Headers);
+        //    if (accinfo.status != responseStatus.ok) return accinfo;
+        //    var ret = new hisdatares
+        //    {
+        //        status = 0,
+        //        hisdata = new List<onedata>(),
+        //          yearOverYeardata = new List<onedata>(),
+        //        linkRelativedata = new List<onedata>()
+        //    };
+        // //   var today = DateTime.Now.ToString("yyyy-MM-dd");
+        //    try
+        //    {
+        //        ret.hisdata = gethisdata(start, end, accinfo.unitid,reportname);
+        //        ret.yearOverYeardata = gethisdata(start.AddYears(-1), end.AddYears(-1), accinfo.unitid, reportname);
+        //        var days = end.Subtract(start).Days;
+        //        ret.linkRelativedata = gethisdata(start.AddDays(-1).AddDays(-days), start.AddDays(-1), accinfo.unitid, reportname);
+        //        return ret;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _log.LogError("{0}-{1}-{2}", DateTime.Now, "GetHistoryData", ex.Message);
+        //        return new commonresponse { status = responseStatus.processerror, content = ex.Message };
+        //    }
+        //}     
+        //private List<onedata> gethisdata(DateTime sd,DateTime  ed,string unitid,string reportname)
+        //{
+        //    var start = sd.ToString("yyyy-MM-dd");
+        //    var end = ed.ToString("yyyy-MM-dd");
+        //    var ret = new List<onedata>();
+        //    var data = _db1.Reportsdata.Where(c => c.Date.CompareTo(start) >= 0
+        // && c.Date.CompareTo(end) <= 0&& c.Rname==reportname
+        // && c.Unitid == unitid);
+        //    foreach (var d in data)
+        //    {
+        //        var one = new onedata();
+        //        one = JsonConvert.DeserializeObject<onedata>(d.Content);
+        //        one.date = d.Date;
+        //        one.submittime = d.Submittime.GetValueOrDefault();
+        //        one.createtime = d.Time;
+        //        ret.Add(one);
+        //    }
+        //    return ret;
+        //}
         private List<onedata> gethisdatanine(DateTime sd, DateTime ed, string unitid, signtype st,string reportname)
         {
             var start = sd.ToString("yyyy-MM-dd");
             var end = ed.ToString("yyyy-MM-dd");
             var ret = new List<onedata>();
             var data = _db1.Reportsdata.Where(c => c.Date.CompareTo(start) >= 0
-         && c.Date.CompareTo(end) <= 0 &&c.Rname==reportname
+         && c.Date.CompareTo(end) <= 0 
+         //&&c.Rname==reportname
          && c.Unitid == unitid);
-            if (st != signtype.unknown) data.Where(c => c.Signtype ==(short) st);
+            if (st != signtype.unknown) data=data.Where(c => c.Signtype ==(short) st);
+            if (!string.IsNullOrEmpty(reportname)) data = data.Where(c => c.Rname == reportname);
             foreach (var d in data)
             {
                 var one = new onedata();
