930bd8d7caa9cdc0e138f062103dcb1597503a56
hedulewei <yang_clin@aliyun.com>
2017-11-08 14:50:50 +0800

    1182

--- trafficpolice/Controllers/checkController.cs
+++ trafficpolice/Controllers/checkController.cs
@@ -28,9 +28,9 @@ namespace trafficpolice.Controllers
         {
             _log = log;
         }
-        [Route("checkFourAgree")]
-        [HttpGet]//4点数据审核同意
-        public commonresponse checkFourAgree(string unitid,string reportname="four")
+        [Route("checkDataAgree")]
+        [HttpGet]//数据审核同意
+        public commonresponse checkDataAgree(string unitid,string reportname="",string date="")
         {
             if (string.IsNullOrEmpty(unitid))
             {
@@ -38,8 +38,13 @@ namespace trafficpolice.Controllers
             }
             var accinfo = global.GetInfoByToken(Request.Headers);
             if (accinfo.status != responseStatus.ok) return accinfo;
-
-            var today = DateTime.Now.ToString("yyyy-MM-dd");
+            var todayd = DateTime.Now;
+           
+            if(date!=""&&!DateTime.TryParse(date,out todayd))
+            {
+                return global.commonreturn(responseStatus.requesterror);
+            }
+            var today = todayd.ToString("yyyy-MM-dd");
             try
             {
                 var unit = _db1.Unit.FirstOrDefault(c => c.Id == accinfo.unitid);
@@ -64,59 +69,22 @@ namespace trafficpolice.Controllers
             }
             catch (Exception ex)
             {
-                _log.LogError("{0}-{1}-{2}", DateTime.Now, "checkFourAgree", ex.Message);
+                _log.LogError("{0}-{1}-{2}", DateTime.Now, "checkDataAgree", ex.Message);
                 return new commonresponse { status = responseStatus.processerror, content = ex.Message };
             }
         }
-        [Route("checkNineAgree")]
-        [HttpGet]//9点数据审核同意
-        public commonresponse checkNineAgree(string   unitid)
-        {
-            if (string.IsNullOrEmpty(unitid))
-            {
-                return global.commonreturn(responseStatus.requesterror);
-            }
-            var accinfo = global.GetInfoByToken(Request.Headers);
-            if (accinfo.status != responseStatus.ok) return accinfo;
-
-            var today = DateTime.Now.ToString("yyyy-MM-dd");
-            try
-            {
-                var unit = _db1.Unit.FirstOrDefault(c => c.Id == accinfo.unitid);
-                if (unit == null)
-                {
-                    return global.commonreturn(responseStatus.nounit);
-                }
-                if (unit.Level==1)
-                {
-                    return global.commonreturn(responseStatus.forbidden);
-                }
-                var data = _db1.Reportsdata.FirstOrDefault(c => c.Date==today
-                && c.Unitid == unitid
-               );
-                if (data == null)
-                {
-                    return global.commonreturn(responseStatus.nounit);
-                }
-                data.Draft = 3;
-                _db1.SaveChanges();
-                return global.commonreturn(responseStatus.ok);
-            }
-            catch (Exception ex)
-            {
-                _log.LogError("{0}-{1}-{2}", DateTime.Now, "checkNineAgree", ex.Message);
-                return new commonresponse { status = responseStatus.processerror, content = ex.Message };
-            }
-        }
+      
 
         public class dvsreq
         {
             public string unitid { get; set; }
+            public string date { get; set; }//为空代表当天
+            public string reportname { get; set; }
             public string comment { get; set; }
         }
-        [Route("checkFourReject")]
-        [HttpPost]//4点数据审核拒绝
-        public commonresponse checkFourReject([FromBody] dvsreq input)
+        [Route("checkDataReject")]
+        [HttpPost]//数据审核拒绝
+        public commonresponse checkDataReject([FromBody] dvsreq input)
         {
             try
             {
@@ -131,8 +99,15 @@ namespace trafficpolice.Controllers
                 {
                     return global.commonreturn(responseStatus.forbidden);
                 }
-                var today = DateTime.Now.ToString("yyyy-MM-dd");
-                var thevs = _db1.Reportsdata.FirstOrDefault(c => c.Date == today && c.Unitid == input.unitid);
+                var todayd = DateTime.Now;
+
+                if (string.IsNullOrEmpty(input. date) && !DateTime.TryParse(input.date, out todayd)||string.IsNullOrEmpty(input.reportname))
+                {
+                    return global.commonreturn(responseStatus.requesterror);
+                }
+                var today = todayd.ToString("yyyy-MM-dd");
+                var thevs = _db1.Reportsdata.FirstOrDefault(c => c.Date == today && c.Unitid == input.unitid
+                &&c.Rname==input.reportname);
                 if (thevs == null)
                 {
                     return global.commonreturn(responseStatus.nounit);
@@ -147,46 +122,46 @@ namespace trafficpolice.Controllers
             }
             catch (Exception ex)
             {
-                _log.LogError("{0}-{1}-{2}", DateTime.Now, "checkFourReject", ex.Message);
+                _log.LogError("{0}-{1}-{2}", DateTime.Now, "checkDataReject", ex.Message);
                 return new commonresponse { status = responseStatus.processerror, content = ex.Message };
             }
         }
-        [Route("DeclineVideoSign")]
-        [HttpPost]
-        public commonresponse DeclineVideoSign([FromBody] dvsreq input)
-        {
-            try
-            {
-                var accinfo = global.GetInfoByToken(Request.Headers);
-                if (accinfo.status != responseStatus.ok) return accinfo;
-                var unit = _db1.Unit.FirstOrDefault(c => c.Id == accinfo.unitid);
-                if (unit == null)
-                {
-                    return global.commonreturn(responseStatus.nounit);
-                }
-                if (unit.Level==1)
-                {
-                    return global.commonreturn(responseStatus.forbidden);
-                }
-                var today = DateTime.Now.ToString("yyyy-MM-dd");
-                var thevs = _db1.Reportsdata.FirstOrDefault(c => c.Date == today && c.Unitid == input.unitid);
-                if (thevs == null)
-                {
-                    return global.commonreturn(responseStatus.nounit);
-                }
-                thevs.Draft = 2;
-                if (!string.IsNullOrEmpty(input.comment))
-                {
-                    thevs.Declinereason = input.comment;
-                }
-                _db1.SaveChanges();
-                return global.commonreturn(responseStatus.ok);
-            }
-            catch (Exception ex)
-            {
-                _log.LogError("{0}-{1}-{2}", DateTime.Now, "DeclineVideoSign", ex.Message);
-                return new commonresponse { status = responseStatus.processerror, content = ex.Message };
-            }
-        }
+        //[Route("DeclineVideoSign")]
+        //[HttpPost]
+        //public commonresponse DeclineVideoSign([FromBody] dvsreq input)
+        //{
+        //    try
+        //    {
+        //        var accinfo = global.GetInfoByToken(Request.Headers);
+        //        if (accinfo.status != responseStatus.ok) return accinfo;
+        //        var unit = _db1.Unit.FirstOrDefault(c => c.Id == accinfo.unitid);
+        //        if (unit == null)
+        //        {
+        //            return global.commonreturn(responseStatus.nounit);
+        //        }
+        //        if (unit.Level==1)
+        //        {
+        //            return global.commonreturn(responseStatus.forbidden);
+        //        }
+        //        var today = DateTime.Now.ToString("yyyy-MM-dd");
+        //        var thevs = _db1.Reportsdata.FirstOrDefault(c => c.Date == today && c.Unitid == input.unitid);
+        //        if (thevs == null)
+        //        {
+        //            return global.commonreturn(responseStatus.nounit);
+        //        }
+        //        thevs.Draft = 2;
+        //        if (!string.IsNullOrEmpty(input.comment))
+        //        {
+        //            thevs.Declinereason = input.comment;
+        //        }
+        //        _db1.SaveChanges();
+        //        return global.commonreturn(responseStatus.ok);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _log.LogError("{0}-{1}-{2}", DateTime.Now, "DeclineVideoSign", ex.Message);
+        //        return new commonresponse { status = responseStatus.processerror, content = ex.Message };
+        //    }
+        //}
     }
 }
