4901a7f3d26154106bccd80d95bf17238c0c26aa
hedulewei <yang_clin@aliyun.com>
2017-11-17 10:54:08 +0800

    some trash data

--- trafficpolice/Controllers/centerController.cs
+++ trafficpolice/Controllers/centerController.cs
@@ -30,10 +30,10 @@ namespace trafficpolice.Controllers
             _log = log;
         }
 
-      [Route("centerGetHistoryData")]//指挥中心查询交管动态历史数据接口
+        [Route("centerGetHistoryData")]//指挥中心查询交管动态历史数据接口
         [HttpGet]
         public commonresponse centerGetHistoryData(string startdate, string enddate,
-            unittype ut=unittype.unknown,signtype st=signtype.unknown, string rname = "")
+              unittype ut = unittype.unknown, signtype st = signtype.unknown, string rname = "")
         {
             var start = DateTime.Now.AddYears(-100);
             var end = DateTime.Now;
@@ -60,11 +60,11 @@ namespace trafficpolice.Controllers
                // && c.Rname==rname
                );
                 if (!string.IsNullOrEmpty(rname)) data = data.Where(c => c.Rname == rname);
-                if (st != signtype.unknown && st!=signtype.all) data = data.Where(c => c.Signtype == (short)st);
+                if (st != signtype.unknown && st != signtype.all) data = data.Where(c => c.Signtype == (short)st);
                 if (ut != unittype.unknown && ut != unittype.all) data = data.Where(c => c.Unitid == ut.ToString());
                 foreach (var d in data)
                 {
-                  var  one = JsonConvert.DeserializeObject<videosigndata>(d.Content);
+                    var one = JsonConvert.DeserializeObject<videosigndata>(d.Content);
                     one.createtime = d.Time;
                     one.submittime = d.Submittime;
                     one.draft = d.Draft;
@@ -82,14 +82,15 @@ namespace trafficpolice.Controllers
         }
         [Route("GetVideoSignData")]
         [HttpGet]//指挥中心视频签到数据获取接口
-        public commonresponse GetVideoSignData(string reportname="nine")
+        public commonresponse GetVideoSignData(string reportname = "nine")
         {
             var accinfo = global.GetInfoByToken(Request.Headers);
             if (accinfo.status != responseStatus.ok) return accinfo;
             var ret = new getvideosigndatares
             {
-                status = 0,signcount=0,
-                vsdata = new List<centerdata> ()
+                status = 0,
+                signcount = 0,
+                vsdata = new List<centerdata>()
             };
             var today = DateTime.Now.ToString("yyyy-MM-dd");
             try
@@ -99,14 +100,14 @@ namespace trafficpolice.Controllers
                 {
                     return global.commonreturn(responseStatus.nounit);
                 }
-                if (unit.Level==1)
+                if (unit.Level == 1)
                 {
                     return global.commonreturn(responseStatus.forbidden);
                 }
-                var data = _db1.Reportsdata.Where(c => c.Date== today && c.Rname==reportname);
-                foreach(var d in data)
+                var data = _db1.Reportsdata.Where(c => c.Date == today && c.Rname == reportname);
+                foreach (var d in data)
                 {
-                    var a= JsonConvert.DeserializeObject<centerdata>(d.Content);
+                    var a = JsonConvert.DeserializeObject<centerdata>(d.Content);
                     a.draft = d.Draft;
                     a.unitid = d.Unitid;
                     //a.date = d.Date;
@@ -114,7 +115,7 @@ namespace trafficpolice.Controllers
                     if (d.Draft == 4) ret.signcount++;
                     ret.vsdata.Add(a);
                 }
-              
+
                 return ret;
             }
             catch (Exception ex)
@@ -124,14 +125,14 @@ namespace trafficpolice.Controllers
             }
         }
 
-      
+
         [Route("SubmitVideoSign")]
         [HttpPost]//指挥中心确认视频签到
-        public commonresponse SubmitVideoSign([FromBody] videosignreq input )
-        {           
+        public commonresponse SubmitVideoSign([FromBody] videosignreq input)
+        {
             try
             {
-                if (input == null||input.videodata == null)
+                if (input == null || input.videodata == null)
                 {
                     return global.commonreturn(responseStatus.requesterror);
                 }
@@ -143,47 +144,55 @@ namespace trafficpolice.Controllers
                 {
                     return global.commonreturn(responseStatus.nounit);
                 }
-                if (unit.Level==1)
+                if (unit.Level == 1)
                 {
                     return global.commonreturn(responseStatus.forbidden);
                 }
                 var today = DateTime.Now.ToString("yyyy-MM-dd");
                 foreach (var d in input.videodata)
                 {
-                    var comment=string.IsNullOrEmpty(d.comment)?string.Empty: d.comment;
+                    if (d.unitid == unittype.all.ToString() || d.unitid == unittype.unknown.ToString() || d.unitid == unittype.center.ToString()) continue;
+                    var comment = string.IsNullOrEmpty(d.comment) ? string.Empty : d.comment;
                     var thed = _db1.Reportsdata.FirstOrDefault(c => c.Date == today && c.Unitid == d.unitid
-                    && c.Rname==d.reportname);
+                    && c.Rname == d.reportname);
                     if (thed != null)
                     {
                         //_log.LogError("data {0} ,{1},{2},not found match, discarded", d.reportname, d.unitid, today);
                         //continue;
-                    
-                    if (d.signtype == signtype.unknown) continue;
-                    thed.Signtype =(short) d.signtype;
-                    thed.Draft = 4;//签到
+
+                        if (d.signtype == signtype.unknown) continue;
+                        thed.Signtype = (short)d.signtype;
+                        thed.Draft = 4;//签到
                         thed.Comment = comment;
-                    //  thed.Content = JsonConvert.SerializeObject(d);
-                }
+                        //  thed.Content = JsonConvert.SerializeObject(d);
+                    }
                     else
                     {
                         _db1.Reportsdata.Add(new Reportsdata
                         {
-                            Date = today, Unitid = d.unitid, Content = string.Empty,Draft=4,Time=DateTime.Now,
-                            Comment=comment,Signtype= (short)d.signtype,Submittime=DateTime.Parse("2222-2-2"),Rname=d.reportname
+                            Date = today,
+                            Unitid = d.unitid,
+                            Content = string.Empty,
+                            Draft = 4,
+                            Time = DateTime.Now,
+                            Comment = comment,
+                            Signtype = (short)d.signtype,
+                            Submittime = DateTime.Parse("2222-2-2"),
+                            Rname = d.reportname
                         });
                     }
-                _db1.SaveChanges();
-                }              
-                
+                    _db1.SaveChanges();
+                }
+
                 return global.commonreturn(responseStatus.ok);
             }
             catch (Exception ex)
             {
-                _log.LogError("{0}-{1}-{2}", DateTime.Now, "SubmitVideoSign", ex.Message);
-                return new commonresponse { status = responseStatus.processerror, content = ex.Message };
+                _log.LogError("{0}-{1}-{2}", DateTime.Now, "SubmitVideoSign", ex.Message + ex.InnerException);
+                return new commonresponse { status = responseStatus.processerror, content = ex.Message+ex.InnerException };
             }
         }
 
-      
+
     }
 }
